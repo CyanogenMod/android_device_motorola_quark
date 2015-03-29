d1f8b8466f5e87a92c40b5b02c5f22abc2f39402
Skrilax_CZ <skrilax@gmail.com>
2015-03-29 22:06:23 +0300

    quark: Add doze for IR Gestore sensors
    Cleanup the sensor code a little bit
    
    Change-Id: I4bd7aa47a410d7c553f42f6b42cd86c44ac4d817

--- cmactions/src/org/cyanogenmod/cmactions/IrGestureSensor.java
+++ cmactions/src/org/cyanogenmod/cmactions/IrGestureSensor.java
@@ -24,22 +24,51 @@ import android.util.Log;
 public class IrGestureSensor implements ActionableSensor, SensorEventListener {
     private static final String TAG = "CMActions-IRGestureSensor";
 
+    // Something occludes the sensor
+    public static final int IR_GESTURE_OBJECT_DETECTED             = 1;
+    // No occlusion
+    public static final int IR_GESTURE_GESTURE_OBJECT_NOT_DETECTED = 2;
+    // Swiping above the phone (send doze)
+    public static final int IR_GESTURE_SWIPE                       = 3;
+    // Hand wave in front of the phone (send doze)
+    public static final int IR_GESTURE_APPROACH                    = 4;
+    // Gestures not tracked
+    public static final int IR_GESTURE_COVER                       = 5;
+    public static final int IR_GESTURE_DEPART                      = 6;
+    public static final int IR_GESTURE_HOVER                       = 7;
+    public static final int IR_GESTURE_HOVER_PULSE                 = 8;
+    public static final int IR_GESTURE_PROXIMITY_NONE              = 9;
+    public static final int IR_GESTURE_HOVER_FIST                  = 10;
+
     private SensorHelper mSensorHelper;
-    private SensorAction mSensorAction;
+    private SensorAction mDozeAction;
+    private Sensor mSensor;
+    private boolean mIsScreenOn;
 
-    private Sensor sensor;
+    private int mLastEventId;
+    private int mGesture = IR_GESTURE_GESTURE_OBJECT_NOT_DETECTED;
 
-    public IrGestureSensor(SensorHelper sensorHelper, SensorAction action) {
+    public IrGestureSensor(SensorHelper sensorHelper, SensorAction dozeAction) {
         mSensorHelper = sensorHelper;
-        mSensorAction = action;
+        mDozeAction = dozeAction;
 
-        sensor = sensorHelper.getIrGestureSensor();
+        mSensor = sensorHelper.getIrGestureSensor();
+    }
+
+    public void setScreenOn(boolean isScreenOn) {
+       mIsScreenOn = isScreenOn;
+       Log.d(TAG, "mIsScreenOn: " + isScreenOn);
+    }
+
+    private void reset(int newEventId) {
+        mGesture = IR_GESTURE_GESTURE_OBJECT_NOT_DETECTED;
+        mLastEventId = newEventId;
     }
 
     @Override
     public void enable() {
         Log.d(TAG, "Enabling");
-        mSensorHelper.registerListener(sensor, this);
+        mSensorHelper.registerListener(mSensor, this);
     }
 
     @Override
@@ -50,9 +79,25 @@ public class IrGestureSensor implements ActionableSensor, SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Log.d(TAG, "event: [" + event.values.length + "]: " + event.values[0] + ", " +
-            event.values[1] + ", " + event.values[2]);
-        mSensorAction.action();
+        Log.d(TAG, "events num: " + event.values.length);
+        for (int i = 0; i < event.values.length; i++) {
+            Log.d(TAG, "event[" + i + "]: " + event.values[i]);
+        }
+
+        int eventId = (int)event.values[0];
+        boolean newEvent = true;
+        if (eventId != mLastEventId) {
+            reset(eventId);
+            newEvent = false;
+        }
+
+        mGesture = (int)event.values[1];
+
+        if (!mIsScreenOn && newEvent &&
+            (mGesture == IR_GESTURE_SWIPE || mGesture == IR_GESTURE_APPROACH) ) {
+            Log.d(TAG, "mGesture: " + mGesture);
+            mDozeAction.action();
+        }
     }
 
     @Override
