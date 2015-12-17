2e1b616bdab0b8c2ed74f01c0973ed0ec0426cd7
Skrilax_CZ <skrilax@gmail.com>
2015-12-17 02:12:37 -0800

    quark: Remove user aware display feature
    This feature was experimental and not working properly,
    as the detection is not reliable enough, causing unwanted
    persistent screen on time when the phone was put on a table
    or below a plant when charging.
    
    Change-Id: Ic8985ec6783d9a90c2be2189b69a2735d9c7ff67

--- cmactions/src/com/cyanogenmod/settings/device/CMActionsSettings.java
+++ cmactions/src/com/cyanogenmod/settings/device/CMActionsSettings.java
@@ -31,7 +31,6 @@ public class CMActionsSettings {
     private static final int ACTION_LAUNCH_CAMERA = 1;
     private static final int ACTION_TORCH = 2;
 
-    private static final String GESTURE_USER_AWARE_DISPLAY_KEY = "gesture_user_aware_display";
     private static final String GESTURE_CAMERA_ACTION_KEY = "gesture_camera_action";
     private static final String GESTURE_CHOP_CHOP_KEY = "gesture_chop_chop";
     private static final String GESTURE_FEEDBACK_INTENSITY_KEY = "gesture_feedback_intensity";
@@ -42,7 +41,6 @@ public class CMActionsSettings {
     private final Context mContext;
     private final UpdatedStateNotifier mUpdatedStateNotifier;
 
-    private boolean mUserAwareDisplayEnabled;
     private int mCameraGestureAction;
     private int mChopChopAction;
     private int mFeedbackIntensity;
@@ -74,9 +72,6 @@ public class CMActionsSettings {
         return new ConfigurableAction(false);
     }
 
-    public boolean isUserAwareDisplayEnabled() {
-        return mUserAwareDisplayEnabled;
-    }
 
     public boolean isCameraGestureEnabled() {
         return mCameraGestureAction != ACTION_NONE;
@@ -107,7 +102,6 @@ public class CMActionsSettings {
     }
 
     private void loadPreferences(SharedPreferences sharedPreferences) {
-        mUserAwareDisplayEnabled = sharedPreferences.getBoolean(GESTURE_USER_AWARE_DISPLAY_KEY, false);
         mCameraGestureAction = getIntPreference(sharedPreferences, GESTURE_CAMERA_ACTION_KEY);
         mChopChopAction = getIntPreference(sharedPreferences, GESTURE_CHOP_CHOP_KEY);
         mFeedbackIntensity = getIntPreference(sharedPreferences, GESTURE_FEEDBACK_INTENSITY_KEY);
@@ -135,9 +129,7 @@ public class CMActionsSettings {
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
             boolean updated = true;
 
-            if (GESTURE_USER_AWARE_DISPLAY_KEY.equals(key)) {
-                mUserAwareDisplayEnabled = sharedPreferences.getBoolean(GESTURE_USER_AWARE_DISPLAY_KEY, false);
-            } else if (GESTURE_CAMERA_ACTION_KEY.equals(key)) {
+            if (GESTURE_CAMERA_ACTION_KEY.equals(key)) {
                 mCameraGestureAction = getIntPreference(sharedPreferences, GESTURE_CAMERA_ACTION_KEY);
             } else if (GESTURE_CHOP_CHOP_KEY.equals(key)) {
                 mChopChopAction = getIntPreference(sharedPreferences, GESTURE_CHOP_CHOP_KEY);
